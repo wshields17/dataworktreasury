0a44fd70fb8204768e117df1f4e567e0035a915c
Walt Shields <wshields1711@gmail.com>
2018-08-29 07:40:23 -0500

    cmpvol

--- addfiles.py
+++ addfiles.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 str = "10.csv"
 sp = pd.read_csv(str)
 str2 = "5.csv"
@@ -10,5 +11,9 @@ str3 = "30.csv"
 sp3 = pd.read_csv(str3)
 res2= pd.merge(res,sp3,how = 'inner', on = 'Date')
 
-print(res2.head())
-res2.to_csv('treasury.csv')
+
+#res2.to_csv('treasury.csv')
+#xcorr = res2[float('10Change %')].corr(res2[float('5Change %')])
+res2['chng'] = (res2['30Price'] - res2['30Price'].shift(-1))/res2['30Price'] 
+yy = res2.loc[:,"10Price"].std()
+print(res2['chng'].rolling(25).std() * math.sqrt(252))
